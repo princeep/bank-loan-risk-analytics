56e378422a0e4ab1b67d28789f45246a231d217b
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-10 22:54:28 +0530

    Refactor SQL queries for loan report analysis
    Refactored SQL queries for better clarity and structure, including month-wise loan applications, funded amounts, and received amounts. Enhanced comments to describe the purpose of each query.

--- BANK_LOAN_REPORT.sql
+++ BANK_LOAN_REPORT.sql
@@ -7,20 +7,31 @@ select * from financial_loan;
 select count(id) as Total_application from financial_loan;
 
 -- Month wise loan application  
-select count(id) total_application from financial_loan where month(issue_date) = 05 ;
+SELECT 
+    MONTHNAME(issue_date) AS month_name,
+    COUNT(*) AS total_loan_applications
+FROM financial_loan
+GROUP BY MONTH(issue_date), MONTHNAME(issue_date)
+ORDER BY MONTH(issue_date);
 
--- Total_Funded Amount
-select sum(loan_amount) as `Total funded amount`,
-	   min(loan_amount) as `min funded amount`,
-       max(loan_amount) as `max funded amount`,
-       avg(loan_amount) as `avg funded amount`
- from financial_loan;
+-- What is the total, minimum, maximum, and average funded loan amount across all loan applications?
+SELECT 
+    SUM(loan_amount) AS `Total Funded Amount`,
+    MIN(loan_amount) AS `Minimum Funded Amount`,
+    MAX(loan_amount) AS `Maximum Funded Amount`,
+    AVG(loan_amount) AS `Average Funded Amount`
+FROM financial_loan;
 
 -- month wise total funded amount
-select sum(loan_amount) as `total funded amount` from financial_loan
-where month(issue_date) = 12;
+SELECT 
+    MONTHNAME(issue_date) AS month,
+    sum(loan_amount) AS total_loan_amount
+FROM
+    financial_loan
+GROUP BY MONTH(issue_date) , MONTHNAME(issue_date)
+ORDER BY MONTH(issue_date);
 
--- Total Resive Amount
+-- What is the total, minimum, maximum, average, and number of loan payments received from customers?
 select sum(total_payment) as `Total esive amount`,
 min(total_payment) as `min resived amount`,
 max(total_payment) as `max resived amount`,
@@ -29,122 +40,160 @@ count(total_payment) as `Number of Payment`
  from financial_loan;
 
 select * from financial_loan;
--- month wise resive amount
-select sum(total_payment) as "month wise resive",
-		min(total_payment) as "Min payment",
-        max(total_payment) as "Max payment"
- from financial_loan
-where month(issue_date)  = 12;
+-- What are the total, minimum, and maximum loan payments received in December
+	SELECT 
+    SUM(total_payment) AS `Total Received Amount`,
+    MIN(total_payment) AS `Minimum Payment`,
+    MAX(total_payment) AS `Maximum Payment`
+FROM financial_loan
+WHERE MONTH(issue_date) = 12;
 
--- average intrest rate
-select avg(int_rate)*100 as avg_intrest_rate from financial_loan;
-
---  avg DTI 
-select avg(dti)*100 as avg_dti from financial_loan;
-
--- Good Loan Percentage
-select (count(case when loan_status = "Fully Paid" or loan_status = "Current" then id end) * 100)/count(id) as Good_loan_per
-from financial_loan;
-
--- Bad Loan Percentage
-select (count(case when loan_status ="Charged Off" then id end) *100)/count(id) as Bed_loan_per from financial_loan;
-
--- Good Loan Application
-select count(id) as Good_loan_application from financial_loan
-where loan_status = "Fully Paid" or loan_status = "Current";
-
--- Good Loan Funded Amount
-select sum(loan_amount) as Good_loan_amount from financial_loan
-where loan_status = "Fully Paid" or loan_status = "Current";  
-
--- Good Loan amount Received
-select sum(total_payment) as Good_loan_amount_resived from financial_loan
-where loan_status = "Fully Paid" or loan_status = "Current";
-
--- Bad Loan Application
-select count(id) as Bad_loan_application from financial_loan
-where loan_status ="Charged Off";
-
--- Bad Loan funded
-select sum(loan_amount) as Bad_loan_funded from financial_loan
-where loan_status = "Charged Off";
-
--- Bad Loan Resived
-select sum(total_payment) as Bad_loan_resived from financial_loan
-where loan_status = "Charged Off"; 
-
---  Loan status
-select loan_status as Loan_status,
-count(id) as Total_application,
-sum(loan_amount) as Total_loan_amt,
-sum(total_payment) as Total_payment,
-AVG(int_rate * 100) AS Interest_Rate
-from financial_loan
-group by loan_status;
-
-
--- Bank Loan Report Overview
+-- What is the average interest rate charged across all loan applications
 SELECT 
-    MONTH(issue_date) AS Month, 
-    MONTHNAME(issue_date) AS Month_name,
-    COUNT(id) AS Total_application,
-    SUM(loan_amount) AS total_Loan_amount,
-    SUM(total_payment) AS Total_received
-FROM 
-    financial_loan
-GROUP BY 
-    MONTH(issue_date), MONTHNAME(issue_date)
-ORDER BY 
-    MONTH(issue_date);
+    AVG(int_rate) * 100 AS average_interest_rate
+FROM financial_loan;
+
+-- What is the average Debt-to-Income (DTI) ratio across all loan applicants
+SELECT 
+    ROUND(AVG(dti) * 100, 2) AS average_dti_ratio
+FROM financial_loan;
+
+-- What percentage of total loans are classified as good loans
+	SELECT 
+    ROUND(
+        (COUNT(
+            CASE 
+                WHEN loan_status IN ('Fully Paid', 'Current') 
+                THEN id 
+            END
+        ) * 100.0) / COUNT(id), 
+    2) AS good_loan_percentage
+FROM financial_loan;
+
+--  What percentage of total loans are classified as bad loans?
+SELECT 
+    ROUND(
+        (COUNT(CASE WHEN loan_status = 'Charged Off' THEN id END) * 100.0) 
+        / COUNT(id), 2
+    ) AS Bad_Loan_Percentage
+FROM financial_loan;
 
 
--- State wise 
-select 
-	address_state as State,
-    count(id) as Total_application,
-    sum(loan_amount) as Total_funded_amount,
-    sum(total_payment) as Total_amount_resived
-    from financial_loan
-    group by address_state
-    order by address_state;
-
--- Term
-select term as Term,
-count(id) as Total_application,
-sum(loan_amount) as Total_funded_amount,
-sum(total_payment) as Total_resived
-from financial_loan
-group by term
-order by term;
+--  How many loan applications are classified as good loans?
+SELECT 
+    COUNT(id) AS Good_Loan_Applications
+FROM financial_loan
+WHERE loan_status IN ('Fully Paid', 'Current');
 
 
--- Employee Lenght
-select emp_length as Emp_Length,
-count(id) as Total_application,
-sum(loan_amount) as Total_funded_amount,
-sum(total_payment) as Total_resived_amount
-from financial_loan 
-group by emp_length
-order by emp_length asc;
-
-select * from financial_loan;
--- Purpose
-select 
-purpose as Purpose,
-count(id) as Total_application,
-sum(loan_amount) as Total_funded_amount,
-sum(total_payment) as Total_resived_amount
-from financial_loan 
-group by purpose
-order by purpose; 
+--  What is the total funded amount for good loans?
+SELECT 
+    SUM(loan_amount) AS Good_Loan_Funded_Amount
+FROM financial_loan
+WHERE loan_status IN ('Fully Paid', 'Current');
 
 
--- Home Ownership
-  SELECT 
-	home_ownership AS `Home Ownership`, 
-	COUNT(id) AS `Total Loan Applications`,
-	SUM(loan_amount) AS `Total Funded Amount`,
-	SUM(total_payment) AS `Total Amount Received`    
+--  What is the total amount received from good loans?
+SELECT 
+    SUM(total_payment) AS Good_Loan_Received_Amount
+FROM financial_loan
+WHERE loan_status IN ('Fully Paid', 'Current');
+
+
+--  How many loan applications are classified as bad loans?
+SELECT 
+    COUNT(id) AS Bad_Loan_Applications
+FROM financial_loan
+WHERE loan_status = 'Charged Off';
+
+
+--  What is the total funded amount for bad loans?
+SELECT 
+    SUM(loan_amount) AS Bad_Loan_Funded_Amount
+FROM financial_loan
+WHERE loan_status = 'Charged Off';
+
+
+--  What is the total amount received from bad loans?
+SELECT 
+    SUM(total_payment) AS Bad_Loan_Received_Amount
+FROM financial_loan
+WHERE loan_status = 'Charged Off';
+
+
+--  What is the loan performance summary by loan status?
+SELECT 
+    loan_status AS Loan_Status,
+    COUNT(id) AS Total_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Received_Amount,
+    ROUND(AVG(int_rate) * 100, 2) AS Average_Interest_Rate
+FROM financial_loan
+GROUP BY loan_status;
+
+
+--  What is the month-wise loan application, funded amount, and received amount trend?
+SELECT 
+    MONTH(issue_date) AS Month_No,
+    MONTHNAME(issue_date) AS Month_Name,
+    COUNT(id) AS Total_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Received_Amount
+FROM financial_loan
+GROUP BY MONTH(issue_date), MONTHNAME(issue_date)
+ORDER BY MONTH(issue_date);
+
+
+--  What is the state-wise loan application and funding analysis?
+SELECT 
+    address_state AS State,
+    COUNT(id) AS Total_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Received_Amount
+FROM financial_loan
+GROUP BY address_state
+ORDER BY address_state;
+
+
+--  What is the loan analysis based on loan term?
+SELECT 
+    term AS Loan_Term,
+    COUNT(id) AS Total_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Received_Amount
+FROM financial_loan
+GROUP BY term
+ORDER BY term;
+
+
+--  What is the loan analysis based on employee experience?
+SELECT 
+    emp_length AS Employee_Length,
+    COUNT(id) AS Total_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Received_Amount
+FROM financial_loan
+GROUP BY emp_length
+ORDER BY emp_length;
+
+
+--  What is the loan analysis based on loan purpose?
+SELECT 
+    purpose AS Loan_Purpose,
+    COUNT(id) AS Total_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Received_Amount
+FROM financial_loan
+GROUP BY purpose
+ORDER BY purpose;
+
+
+--  What is the loan analysis based on home ownership?
+SELECT 
+    home_ownership AS Home_Ownership,
+    COUNT(id) AS Total_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Received_Amount
 FROM financial_loan
 GROUP BY home_ownership
 ORDER BY home_ownership;
